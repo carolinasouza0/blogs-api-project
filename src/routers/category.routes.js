0e47c14280c54c21392bf456f844729043710ec9
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-11-02 13:44:36 -0300

    feat: create GET endpoint /categories

--- src/routers/category.routes.js
+++ src/routers/category.routes.js
@@ -7,5 +7,6 @@ const validateCategory = require('../middlewares/validateCategory');
 const { validate } = require('../middlewares/validateToken');
 
 router.post('/', validate, validateCategory, categoryController.createCategory);
+router.get('/', validate, categoryController.getAllCategories);
 
 module.exports = router;
